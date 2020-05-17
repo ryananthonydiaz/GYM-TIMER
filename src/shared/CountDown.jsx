5d0ca42a6e706208979ebce5300e8dfbf827877a
Ryan Diaz <ryandiaz@Ryans-MacBook-Pro.local>
2020-05-17 00:09:19 -0700

    timer styled

--- src/shared/CountDown.jsx
+++ src/shared/CountDown.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function StandardRunningClock({ countDown, setCountDown }) {
   const tenSecondCountDown = () => setCountDown(cntDown => cntDown - 1);
+  const [countDownLabel, setCountDownLabel] = useState('')
 
   useEffect(() => {
     let countDownInterval = setInterval(tenSecondCountDown, 1000);
+    setCountDownLabel(`${countDown.toString().padStart(2, '0')}`);
     if (countDown === 0) {
       clearInterval(countDownInterval);
+      setCountDownLabel('');
     }
 
     return () => clearInterval(countDownInterval);
   }, [countDown]);
   return (
-    <div>
-      <p>{countDown.toString().padStart(2, '0')}</p>
-    </div>
+  <div>{countDownLabel}</div>
   );
 }
 
--- src/timers/Timer.jsx
+++ src/timers/Timer.jsx
@@ -1,20 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import CountDown from '../shared/CountDown';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   clock: {
-    fontFamily: 'orbitron',
+    // border: '1px solid white',
+    width: '100vw',
+    fontSize: '5rem',
+    fontWeight: '100',
+    color: 'white',
+    textAlign: 'center',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: '#daf6ff',
+    textShadow: '0 0 20px rgba(10, 175, 230, 1),  0 0 20px rgba(10, 175, 230, 0)',
+    [theme.breakpoints.down('sm')]: {
+      top: '30%',
+      transform: 'translate(-50%, -30%)',
+    },
+  },
+  buttons:  {
+    position: 'absolute',
+    top: '60%',
+    left: '50%',
+    transform: 'translate(-50%)',
+  },
+  button: {
+    margin: theme.spacing(0, 2),
   }
-});
+}));
 
 function StandardRunningClock() {
+  const classes = useStyles();
   const [countDown, setCountDown] = useState(10);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-
-  const time = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 
   const tenSecondCountDown = () => setCountDown(cntDown => cntDown - 1);
 
@@ -33,6 +57,13 @@ function StandardRunningClock() {
     }
   }
 
+  let time;
+  if (countDown === 0) {
+    time = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  } else {
+    time = <CountDown countDown={countDown} setCountDown={setCountDown} />;
+  }
+
   useEffect(() => {
     let stopWatchInterval = null;
 
@@ -44,10 +75,17 @@ function StandardRunningClock() {
   }, [seconds, minutes, countDown]);
 
   return (
-    <div>
-      <CountDown countDown={countDown} setCountDown={setCountDown} />
-      {time}
-    </div>
+    <>
+      <div className={classes.clock}>
+        {time}
+        <div className={classes.buttons}>
+          <Button className={classes.button} onClick={() => console.log('1')}>Start</Button>
+          <Button className={classes.button} onClick={() => console.log('2')}>Stop</Button>
+          <Button className={classes.button} onClick={() => console.log('3')}>Reset</Button>
+        </div>
+      </div>
+
+    </>
   );
 }
 
