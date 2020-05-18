306204390729a10e860ff1ee6bb567cc5c74ede7
Ryan Diaz <ryandiaz@Ryans-MBP.lan1>
2020-05-18 15:32:15 -0700

    Interval component logic initiated

--- src/timers/Interval.jsx
+++ src/timers/Interval.jsx
@@ -1,7 +1,116 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CountDown from '../shared/CountDown';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  clock: {
+    // border: '1px solid white',
+    width: '100vw',
+    fontSize: '10rem',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '5rem',
+    },
+    fontWeight: '100',
+    color: 'white',
+    textAlign: 'center',
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    textShadow: '0 0 20px rgba(10, 175, 230, 1),  0 0 20px rgba(10, 175, 230, 0)',
+    [theme.breakpoints.down('sm')]: {
+      top: '30%',
+      transform: 'translate(-50%, -30%)',
+    },
+  },
+  buttons:  {
+    position: 'absolute',
+    top: '60%',
+    left: '50%',
+    transform: 'translate(-50%)',
+  },
+  button: {
+    margin: theme.spacing(0, 2),
+  }
+}));
 
 function Interval() {
-  return <div>Interval Page</div>
+  const classes = useStyles();
+  const [clockRunning, setClockRunning] = useState(false);
+  const [countDownRunning, setCountDownRunning] = useState(false);
+  const [countDown, setCountDown] = useState(10);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const startClock = () => {
+    // If the clock is paused/stopped at 00:00:00
+    if (hours === 0 && minutes === 0 && seconds === 0 && clockRunning === false) {
+      setCountDownRunning(true)
+      setCountDown(10);
+      setClockRunning(true)
+    } else {
+      setClockRunning(true);
+    }
+  }
+
+  const stopClock = () => {
+    if (countDownRunning === true) {
+      setCountDownRunning(false);
+    }
+
+    setClockRunning(false);
+  }
+
+  const reset = () => {
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+    setCountDown(false);
+    setClockRunning(false)
+  }
+
+  const clockLogic = () => {
+    if (clockRunning === true) {
+      if (seconds === 59) {
+        if (minutes === 59) {
+          setHours(hrs => hrs + 1);
+          setMinutes(0);
+          setSeconds(0);
+          return;
+        }
+        setSeconds(0);
+        setMinutes(mins => mins + 1);
+      } else {
+        setSeconds(secs => secs + 1);
+      }
+    }
+  }
+
+  const clock = `${hours.toString().padStart(2, '0')} : ${minutes.toString().padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`;
+  const tMinus = <CountDown countDown={countDown} setCountDown={setCountDown} />;
+
+  useEffect(() => {
+    let clockInterval = null;
+
+    if (countDown === 0) {
+      clockInterval = setInterval(clockLogic, 1000);
+    }
+
+    return () => clearInterval(clockInterval);
+  }, [seconds, minutes, countDown, clockRunning]);
+
+  return (
+      <div className={classes.clock}>
+        {countDownRunning === true && countDown > 0 ? tMinus : clock}
+        <div className={classes.buttons}>
+          <Button className={classes.button} onClick={startClock}>Start</Button>
+          <Button className={classes.button} onClick={stopClock}>Stop</Button>
+          <Button className={classes.button} onClick={reset}>Reset</Button>
+        </div>
+      </div>
+  );
 }
 
 export default Interval;
